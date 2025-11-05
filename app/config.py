69629e1637ddf020df02ad2c8c21434da016b3be
blakcoder2017 <sherifdata@gmail.com>
2025-11-05 12:11:36 +0000

    added production Postrgres database url

--- app/config.py
+++ app/config.py
@@ -9,6 +9,7 @@ class Settings(BaseSettings):
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
+    prod_database_url: str
 
     class Config:
         env_file = ".env"
